b01665860ec352378cd0cf353827ea6d1825646f
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-08-10 10:20:30 +0100

    perlin

---
+++ perlin.cs
@@ -0,0 +1,27 @@
+public double perlin(double x, double y, double z) {
+    // ...
+
+    double x1, x2, y1, y2;
+    x1 = lerp(    grad (aaa, xf  , yf  , zf),           // The gradient function calculates the dot product between a pseudorandom
+                grad (baa, xf-1, yf  , zf),             // gradient vector and the vector from the input coordinate to the 8
+                u);                                     // surrounding points in its unit cube.
+    x2 = lerp(    grad (aba, xf  , yf-1, zf),           // This is all then lerped together as a sort of weighted average based on the faded (u,v,w)
+                grad (bba, xf-1, yf-1, zf),             // values we made earlier.
+                  u);
+    y1 = lerp(x1, x2, v);
+
+    x1 = lerp(    grad (aab, xf  , yf  , zf-1),
+                grad (bab, xf-1, yf  , zf-1),
+                u);
+    x2 = lerp(    grad (abb, xf  , yf-1, zf-1),
+                  grad (bbb, xf-1, yf-1, zf-1),
+                  u);
+    y2 = lerp (x1, x2, v);
+    
+    return (lerp (y1, y2, w)+1)/2;                      // For convenience we bind the result to 0 - 1 (theoretical min/max before is [-1, 1])
+}
+
+// Linear Interpolate
+public static double lerp(double a, double b, double x) {
+    return a + x * (b - a);
+}
